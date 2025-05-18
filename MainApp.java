54f26708348c3b7615c317977ca60f83e58328fe
Rachmat Tri Aji <ajilewo@gmail.com>
2025-05-18 23:23:13 +0700

    Update MainApp.java
    update yang lama

--- MainApp.java
+++ MainApp.java
@@ -1,26 +1,19 @@
-import java.time.format.DateTimeFormatter;
-
+// MainApp.java
 public class MainApp {
     public static void main(String[] args) {
         // Membuat objek Electronics
-        Electronics laptop = new Electronics("Laptop ASUS ROG", 1200.00, 201, 24, 2.5, "ASUS");
+        Electronics laptop = new Electronics("Laptop ASUS", 899.99, 101, 12);
 
         // Membuat objek Clothing
-        Clothing shirt = new Clothing("Kemeja Flanel", 35.75, 305, Size.M);
+        Clothing shirt = new Clothing("T-Shirt Polos", 25.50, 205, "M");
 
         // Mencetak informasi produk Electronics
         System.out.println("Informasi Produk Elektronik:");
         System.out.println(laptop.getInfo());
-        System.out.println("Kategori: " + laptop.getCategory());
-        System.out.println("Harga setelah diskon 10%: €" + String.format("%.2f", laptop.getDiscountedPrice(10)));
-        System.out.println("Spesifikasi: " + laptop.getSpecification().getSpecs());
         System.out.println();
 
         // Mencetak informasi produk Clothing
         System.out.println("Informasi Produk Pakaian:");
         System.out.println(shirt.getInfo());
-        System.out.println("Kategori: " + shirt.getCategory());
-        System.out.println("Harga setelah diskon 10%: €" + String.format("%.2f", shirt.getDiscountedPrice(10)));
-        System.out.println();
     }
 }
